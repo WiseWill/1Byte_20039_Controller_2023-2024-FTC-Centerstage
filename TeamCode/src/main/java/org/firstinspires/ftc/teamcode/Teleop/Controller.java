6cde0a7416b2d0c729bb5741d1635655f20d2f97
WiseWill <89420689+WiseWill@users.noreply.github.com>
2023-11-18 07:30:25 -0600

    Update Controller.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Controller.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -20,6 +21,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @TeleOp
 public class Controller extends OpMode{
 
+
+
+
+
     private final ElapsedTime runtime = new ElapsedTime();
 
     //declaring motes
@@ -29,20 +34,38 @@ public class Controller extends OpMode{
     private DcMotorEx wheelFR;
     private DcMotorEx wheelBR;
 
-    private DcMotor intakeRight;
-    private DcMotor intakeLeft;
+    private DcMotorEx armSlideMoter;
+
+    private DcMotor intakeMoter;
+
+    private Servo armServo;
+
+
+
+
+
 
 
     private double previousRunTime;
     private double rotation;
 
     //intake on bool
-    private boolean intakeOn;
+    private boolean intakeOn = false;
     private boolean aPressed;
 
 
+    double initialposition;
+    private int minTickSlide = 100;
+    private  int maxTickSlide = 2700;
+
+    private int slideTarget;
+    private boolean armUp = false;
+
+    private boolean armflip = false;
+    private  boolean bpressed = false;
 
 public void init(){
+
  telemetry.addData("Status", "Initialized");
 
 
@@ -52,18 +75,27 @@ public void init(){
     wheelBL = hardwareMap.get(DcMotorEx.class, "wheelBL");
     wheelBR = hardwareMap.get(DcMotorEx.class, "wheelBR");
 
+    armSlideMoter = hardwareMap.get(DcMotorEx.class, "slideMoter");
+
+    armServo = hardwareMap.get(Servo.class, "servoArm");
+
     //adding intake moters.
-    intakeRight = hardwareMap.get(DcMotor.class, "leftInta");
-    intakeLeft = hardwareMap.get(DcMotor.class, "RightInta");
+    intakeMoter = hardwareMap.get(DcMotorEx.class, "IntaMoter");
+
 
     //allowing it to run withought encoders
     wheelFL.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
     wheelFR.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
     wheelBL.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
     wheelBR.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+    armSlideMoter.setTargetPosition(50);
+    armSlideMoter.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-    intakeLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    intakeRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    intakeMoter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
 
 
 
@@ -74,11 +106,18 @@ public void init(){
     wheelBL.setDirection(DcMotorSimple.Direction.FORWARD);
     wheelBR.setDirection(DcMotorSimple.Direction.REVERSE);
 
-    intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
-    intakeLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+    intakeMoter.setDirection(DcMotorSimple.Direction.FORWARD);
+
+    armSlideMoter.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
     //tell driver initialization if complete - do not add vulgarity @isaiah @saiansh
     telemetry.addData("Status","Initialized");
+
+
+    initialposition = armSlideMoter.getCurrentPosition();
+    minTickSlide = (int)(50 + initialposition);
+    maxTickSlide = (int)(2700 + initialposition);
 }
     @Override
     public void init_loop(){} //runs after init but berfore play
@@ -94,6 +133,29 @@ public void init(){
     public void loop(){
         basicDrive();
         intake();
+        armSlide();
+        armflip();
+    }
+
+    private void armflip() {
+        if(gamepad1.b){
+            if(bpressed){
+
+            } else {
+                bpressed = true;
+                armflip = !armflip;
+                telemetry.addData("Status","Flip");
+            }
+        } else {
+            bpressed = false;
+        }
+
+
+        if(armflip){
+            armServo.setPosition(-90);
+        } else {
+            armServo.setPosition(0);
+        }
     }
 
     public void basicDrive(){
@@ -131,13 +193,48 @@ public void init(){
         }
 
         if(intakeOn){
-            intakeRight.setPower(1);
-            intakeLeft.setPower(1);
+            intakeMoter.setPower(1);
         } else {
-            intakeRight.setPower(0);
-            intakeLeft.setPower(0);
+            intakeMoter.setPower(0);
         }
     }
+
+    void armSlide(){
+        //trigger imput
+
+
+        armSlideMoter.setTargetPositionTolerance(50);
+        if(gamepad1.left_bumper){
+            slideTarget = 50;
+        } else if(gamepad1.right_bumper) {
+            slideTarget = 2700;
+        }
+
+        slideTarget += (int)((gamepad1.right_trigger * 25) - (gamepad1.left_trigger * 25));
+
+        if(slideTarget > maxTickSlide) {
+            slideTarget = maxTickSlide;
+        } else if(slideTarget < minTickSlide){
+            slideTarget = minTickSlide;
+        }
+
+        armSlideMoter.setTargetPosition(slideTarget);
+
+        if(armSlideMoter.getCurrentPosition() - slideTarget < 10 && armSlideMoter.getCurrentPosition() - slideTarget > 10){
+            armSlideMoter.setVelocity(0);
+        } else {
+            armSlideMoter.setVelocity(1750);
+        }
+
+
+
+    }
+
+
+
+
+
+
 
 
     public static void wait(int ms) {
